c348ee5b7671134a46c34f6e14c1c3a4e5b0f6b9
Mike Holzinger <mike.holzinger@gmail.com>
2014-04-18 14:06:31 -0700

    Sudo added to Nginx repo config

--- config_nginx.sh
+++ config_nginx.sh
@@ -4,18 +4,18 @@
 SLVER=$(cat /etc/redhat-release | sed -e 's#[^0-9]##g' | cut -c1)
 
 ### Add Nginx repo Scientific Linux
-cat <<EOF >/etc/yum.repos.d/nginx.repo
+sudo bash -c 'cat <<EOF >/etc/yum.repos.d/nginx.repo
 [nginx]
 name=nginx repo
 baseurl=http://nginx.org/packages/centos/$SLVER/\$basearch/
 gpgcheck=0
 enabled=1
-EOF
+EOF'
 
 ### Install latest Nginx repo
-yum update
-yum install nginx
-chkconfig nginx on
-/etc/init.d/nginx start
+sudo yum update
+sudo yum install -y nginx
+sudo chkconfig nginx on
+sudo /etc/init.d/nginx start
 
 
