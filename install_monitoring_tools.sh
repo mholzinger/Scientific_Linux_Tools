f0ddcfec854f2f7afea892db1d39bb182d84eae7
Mike Holzinger <mike.holzinger@gmail.com>
2014-05-04 17:47:59 -0700

    HTOP installed and configured in network monitor tools

--- install_monitoring_tools.sh
+++ install_monitoring_tools.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
-# Install rpm package from rpmforge
-wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-sudo rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm 
+rpmforge()
+{
+    ### RPMForge release version
+    # source rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+    Major=0.5
+    Minor=3-1
+    Rel=$Major.$Minor
+
+    case $1 in
+        install )
+            # install rpmforge
+            cd /tmp
+            wget http://packages.sw.be/rpmforge-release/rpmforge-release-$Rel.el$SLVer.rf.$Arch.rpm
+            sudo rpm -ihv rpmforge-release*.rpm
+            rm -f rpmforge-release*.rpm ;;
+
+        uninstall )
+            # install rpmforge
+            sudo yum remove -y rpmforge-release ;;
+    esac
+}
+
+system_vars()
+{
+    Arch=$( uname -p )
+    # Grab SL version before adding repository
+    SLVer=$(cat /etc/redhat-release | sed -e 's#[^0-9]##g' | cut -c1)
+}
+
+system_vars
+
+rpmforge install
 
 # Install HTOP
-sudo yum -y install htop
+sudo yum install -y htop
+
+# END - Clean up
+
+# Remove rpmforge to prevent repo parsing issue in the future
+rpmforge uninstall
+
 
---
+++ update_epel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo bash -c 'cat <<EOM >/etc/yum.repos.d/epel-bootstrap.repo
+[epel]
+name=Bootstrap EPEL
+mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-\$releasever&arch=\$basearch
+failovermethod=priority
+enabled=0
+gpgcheck=0
+EOM'
+
+yum --enablerepo=epel -y install epel-release
+rm -f /etc/yum.repos.d/epel-bootstrap.repo
+
+
